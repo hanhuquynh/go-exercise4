a5bd3ce786d20dab6d2cef9f60185ba255108703
Quỳnh <hanhuquynh25122001@gmail.com>
2022-07-09 14:06:48 +0700

    exercises4

--- bai2.go
+++ bai2.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"database/sql"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func transaction() {
+	db, err := sql.Open("mysql", "root:hanhuquynh@tcp(127.0.0.1:3306)/test")
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	defer db.Close()
+}
--- main.go
+++ main.go
@@ -1,17 +1,22 @@
 package main
 
 func main() {
-	createDB("test")
+	// createDB("test")
 
-	insertUser(1, "test 1", 2001, 07, 07)
-	insertUser(2, "test 2", 2001, 07, 07)
-	insertUser(3, "test 3", 2001, 07, 07)
-	insertUser(4, "test 4", 2001, 07, 07)
-	insertUser(5, "test 5", 2001, 07, 07)
+	// insertUser(1, "test 1", 2001, 07, 07)
+	// insertUser(2, "test 2", 2001, 07, 07)
+	// insertUser(3, "test 3", 2001, 07, 07)
+	// insertUser(4, "test 4", 2001, 07, 07)
+	// insertUser(5, "test 5", 2001, 07, 07)
 
-	updateUser(1, "HNQ", 2001, 07, 07)
+	// updateUser(1, "HNQ", 2001, 07, 07)
 
-	listUser()
+	// listUser()
 
-	listUserById(1)
+	// listUserById(88)
+
+	// for i := 1; i <= 100; i++ {
+	// 	insertUser(i, "test"+strconv.Itoa(i), 2001, 9, 9)
+	// }
+
 }
